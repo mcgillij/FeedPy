fbf3c3ddc0a0e62ed2cecf565560b37777222be1
Jason McGillivray <mcgillivray.jason@gmail.com>
2012-06-07 02:17:00 -0300

    removed extra files

--- test-pyside.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide.QtCore import *
-from PySide.QtGui import * 
-
-app = QApplication(sys.argv)
-label = QLabel("Hello World")
-label.show()
-app.exec_()
-sys.exit()
--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from PyQt4.QtGui import QApplication, QPushButton
-
-app = QApplication(sys.argv)
-button = QPushButton("Hello World!")
-button.show()
-sys.exit(app.exec_())
