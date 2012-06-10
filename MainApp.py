223711480f162b7d5f3aceb18158681bc0908420
Jason McGillivray <mcgillivray.jason@gmail.com>
2012-06-10 00:01:25 -0300

    changes to the exclusive filters

--- MainApp.py
+++ MainApp.py
@@ -23,6 +23,7 @@ class MainApp(QtGui.QMainWindow, untitled.Ui_MainWindow):
         super(MainApp, self).__init__(parent)
         self.setupUi(self)
         self.active_filter = None
+        self.exclusive_filters = []
         self.settings = Settings()
         self.settings.load_settings()
         self.statusbar.showMessage('Feeds tracked: ' + str(len(self.settings.uri_list)))
@@ -52,13 +53,17 @@ class MainApp(QtGui.QMainWindow, untitled.Ui_MainWindow):
     def generate_filter_buttons(self):
         # clear the hbox of widgets before adding new ones.
         self.active_filter = None
+        self.exclusive_filters = []
         for i in range(self.horizontalLayout.count()):
             self.horizontalLayout.itemAt(i).widget().close()
             
         for f in self.settings.filters:
-            widget = QtGui.QPushButton(QString(f['filter']))
-            widget.clicked.connect(functools.partial(self.filter_on, f))
-            self.horizontalLayout.addWidget(widget)
+            if f['plus']:
+                widget = QtGui.QPushButton(QString(f['filter']))
+                widget.clicked.connect(functools.partial(self.filter_on, f))
+                self.horizontalLayout.addWidget(widget)
+            else:
+                self.exclusive_filters.append(f)
 
     def filter_on(self, filter):
         self.active_filter = filter
@@ -67,10 +72,17 @@ class MainApp(QtGui.QMainWindow, untitled.Ui_MainWindow):
             val = check_for_val(e, filter['filter'])
             if val:
                 new_list.append(e)
+                
         if new_list:
             self.listWidgetRss.clear()
             for entry in new_list:
-                RssListItem(entry, self.listWidgetRss)
+                if self.exclusive_filters:
+                    for f in self.exclusive_filters:
+                        rv = check_for_val(entry, f['filter'])
+                        if rv == False:
+                            RssListItem(entry, self.listWidgetRss)
+                else:
+                    RssListItem(entry, self.listWidgetRss)
 
     def _slotFilters(self):
         return_value = self.filter_dialogue.exec_()
@@ -100,7 +112,13 @@ class MainApp(QtGui.QMainWindow, untitled.Ui_MainWindow):
         self.listWidgetRss.clear()
         self.feed_reader.parse(self.settings.uri_list)
         for entry in self.feed_reader.entries:
-            RssListItem(entry, self.listWidgetRss)
+            if self.exclusive_filters:
+                for f in self.exclusive_filters:
+                    rv = check_for_val(entry, f['filter'])
+                    if rv == False:
+                        RssListItem(entry, self.listWidgetRss)
+            else:
+                RssListItem(entry, self.listWidgetRss)
         self.timer.start(self.settings.refresh_time * 60 * 1000)
         status_string = "Refresh took: " + str(int((time.time()- start_time))) + " seconds."
         self.statusbar.showMessage(status_string, 2000)
